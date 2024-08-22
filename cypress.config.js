3a611b98c95187cc0c829ddbcb13ff711914af4a
emmanuel <emmanuelkosi19@gmail.com>
2024-08-22 07:37:36 +0100

    second commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.lambdatest.com/selenium-playground/",
+    defaultCommandTimeout: 50000,
+    viewportHeight: 900,
+    viewportWidth: 1680,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/download/downloadspec.cy.js
+++ cypress/e2e/download/downloadspec.cy.js
@@ -2,7 +2,13 @@ describe('downloads', () => {
 
   it('Users should be able to download texts', () => {
     cy.fixture('selectors').then((data)=>{
-      
+      cy.visit('/')
+      cy.contains('File Download').should('exist').and('be.visible').click()
+      cy.get(data.textField).should('be.visible').type('Consistency is key')
+      cy.get(data.generateFile).should('be.visible').click()
+      cy.get(data.downloadButton).should('be.visible').click()
+
+
     })
    
   })
